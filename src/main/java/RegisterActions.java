2ef70b6976a99d894983c5a41e20d398ae6d6194
bayurov <bajurovt@gmail.com>
2014-12-10 00:46:25 +0300

    попытка поправить лояут

--- src/main/java/RegisterActions.java
+++ src/main/java/RegisterActions.java
@@ -29,83 +29,101 @@ public class RegisterActions {
 
 //        Регистрация хелпера
         new SimpleAction("help", "Просмотр всех доступных команд 'help' или конкретной 'help exit'", (args) -> {
+
             if (args.isEmpty()) {
                 SimpleAction.actions.values().forEach(triple -> print(triple.first + " - " + triple.second));
                 return;
             }
+
             Triplet<String, String, Action> command = SimpleAction.actions.get(args);
+
             print(command.first + " - " + command.second);
+
         });
 
 //        Регистрация метода вставки
         new SimpleAction("insert", "Вставка клиента", (args) -> {
+
             PersonalData personalData = new PersonalData();
-            print("Введите ФИО");
-            personalData.fio = inNextLine();
-            print("Введите должность");
-            personalData.job = inNextLine();
-            print("Введите e-mail");
-            personalData.email = inNextLine();
-            print("Введите номер телефона");
-            personalData.phone = inNextLine();
+
+            print("Введите ФИО");               personalData.fio = inNextLine();
+            print("Введите должность");         personalData.job = inNextLine();
+            print("Введите e-mail");            personalData.email = inNextLine();
+            print("Введите номер телефона");    personalData.phone = inNextLine();
+
             if (personalData.validate().isEmpty()) {
                 Storage.add(personalData);
                 print("Данные сохранены");
             } else {
                 personalData.getInvalidMessage().stream().forEach(IOHelper::print);
             }
+
         });
 
 //        Регистрация метода обнавления
         new SimpleAction("update", "Обновление клиента", (args) -> {
+
             print("Выберите клиента");
-            int[] index = { 0 };
+
             Persons persons = Storage.all();
+
+            int[] index = { 0 };
+
             persons.personals.stream().forEachOrdered((person) ->  {
-                print(index[0] + ". " + person.fio);
-                index[0]++;
+                print(index[ 0 ] + ". " + person.fio);
+                index[ 0 ]++;
             });
+
             PersonalData personalData = persons.personals.get(Integer.parseInt(inNextLine()));
-            print("Введите ФИО");
-            personalData.fio = inNextLine();
-            print("Введите должность");
-            personalData.job = inNextLine();
-            print("Введите e-mail");
-            personalData.email = inNextLine();
-            print("Введите номер телефона");
-            personalData.phone = inNextLine();
+
+            print("Введите ФИО");               personalData.fio = inNextLine();
+            print("Введите должность");         personalData.job = inNextLine();
+            print("Введите e-mail");            personalData.email = inNextLine();
+            print("Введите номер телефона");    personalData.phone = inNextLine();
+
             if (personalData.validate().isEmpty()) {
                 Storage.updateAll(persons);
                 print("Данные сохранены");
             } else {
                 personalData.getInvalidMessage().stream().forEach(IOHelper::print);
             }
+
         });
 
 //        Регистрация метода обнавления
         new RequireParamAction("delete", "Удаление клиента", (args) -> {
+
             Storage.deleteByFio(args);
+
             print("Удален");
+
         });
 
 //        Регистрация метода простотра
         new SimpleAction("list", "Список клиентов", args -> {
+
             List<PersonalData> persons = Storage.all().personals;
-            if (args.contains("fio"))    persons.sort((a, b) -> a.fio.compareTo(b.fio));
-            if (args.contains("job"))    persons.sort((a, b) -> a.job.compareTo(b.job));
-            if (args.contains("email"))    persons.sort((a, b) -> a.email.compareTo(b.email));
-            if (args.contains("phone"))    persons.sort((a, b) -> a.phone.compareTo(b.phone));
+
+            if (args.contains("fio"))       persons.sort((a, b) -> a.fio.compareTo(b.fio));
+            if (args.contains("job"))       persons.sort((a, b) -> a.job.compareTo(b.job));
+            if (args.contains("email"))     persons.sort((a, b) -> a.email.compareTo(b.email));
+            if (args.contains("phone"))     persons.sort((a, b) -> a.phone.compareTo(b.phone));
+
             persons.forEach(person -> print(person.toString()));
+
         });
 
 //        Регистрация метода поиска
         new RequireParamAction("find", "Поиск клиента по номеру телефона", (args) -> {
+
             PersonalData person = Storage.findByPhone(args);
+
             if (person != null){
                 print(person.toString());
             } else {
               print("По вашему запросу ничего не найдено");
             }
+
         });
 
 //        Регистрация команды выхода из приложения
